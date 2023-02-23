aa88ac71ddd3af2789b3e08e0d1affc34f5d9a39
libvirgo <sakurapetgirl@live.com>
2023-02-23 17:35:50 +0800

    add zsh-nix-shell.

--- users/sakura/home.nix
+++ users/sakura/home.nix
@@ -108,6 +108,17 @@ in
               sha256 = "sha256-yN1qmuwWNkWHF9ujxZq2MiroeASh+KQiCLyK5ellnB8=";
           };
         }
+	{
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.5.0";
+            sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+          };
+        }
+
       ];
       zplug = {
         enable = true;
